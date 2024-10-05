4187e241b257c9c82cc3acd932a067817e86e7c0
MRKart <rafiikart@icloud.com>
2024-10-05 23:22:15 +0300

    Update 3_object_literals_kullanimi.js

--- 3_object_literals_kullanimi.js
+++ 3_object_literals_kullanimi.js
@@ -1,5 +1,90 @@
 //! Object Literals
 
+JavaScript te object (nesne), birden fazla değeri tek bir yapıda saklamamızı sağlayan 
+veri türüdür. Her değerin bir anahtarı (key) ve değeri (value) vardır. 
+Object literals ise JavaScript te nesne oluşturmanın en basit yoludur; 
+süslü parantezler {} içinde anahtar-değer çiftleriyle nesne tanımlanır.
+
+//! Object Literals ile Nesne Oluşturma :
+Object literals, anahtar-değer çiftlerini kullanarak nesneleri tanımlamak için kullanılır. 
+Aşağıda basit bir örnek bulunuyor.
+
+  //? Örnek:
+
+    let kisi = {
+      ad: "Ahmet",       // anahtar: ad, değer: "Ahmet"
+      yas: 25,           // anahtar: yas, değer: 25
+      meslek: "Mühendis" // anahtar: meslek, değer: "Mühendis"
+    };
+    
+    Bu örnekte, kisi adlı nesne üç farklı anahtar-değer çiftinden oluşuyor: ad, yas ve meslek.
+
+//! //////////////////////////////////////////////////////////////////////////////////////////
+
+//! Nesneye Erişim:
+Bir nesnenin içerisindeki verilere iki farklı yolla erişebilirsin.
+
+  //! 1. Nokta Notasyonu (.)
+
+    //? Örnek: 
+
+      let kisi = {
+      ad: "Ahmet",
+      yas: 25, 
+      meslek: "Mühendis",
+      };
+    
+      console.log(kisi.ad); // "Ahmet"
+      console.log(kisi.yas); // 25
+  
+  //! ************************************************
+
+  //! 2. Köşeli Parantez Notasyonu ([])
+  Anahtarın bir string olduğu durumlarda veya dinamik bir anahtar kullanıldığında kullanılır.   
+
+    //? Örnek: 
+
+      let kisi = {
+        ad: "Ahmet", // anahtar: ad, değer: "Ahmet"
+        yas: 25, // anahtar: yas, değer: 25
+        meslek: "Mühendis", // anahtar: meslek, değer: "Mühendis"
+      };
+      
+      console.log(kisi["meslek"]); // "Mühendis"
+      
+      let ozellik = "yas";
+      console.log(kisi[ozellik]); // 25
+  
+  //! ************************************************
+
+//! Nesneye Yeni Özellik Ekleme
+Nesneye yeni bir özellik eklemek için basitçe nokta notasyonu veya köşeli parantez 
+notasyonu kullanabilirsin.
+
+    //? Örnek: 
+
+      let kisi = {
+        ad: "Ahmet", // anahtar: ad, değer: "Ahmet"
+        yas: 25, // anahtar: yas, değer: 25
+        meslek: "Mühendis", // anahtar: meslek, değer: "Mühendis"
+      };
+
+      kisi.soyad = "Yılmaz";
+      console.log(kisi.soyad);
+
+    //veya
+    
+
+  //! ************************************************
+
+
+
+
+
+
+
+
+
 // let firstName = "Muhammet";
 // let lastName = "Kart";
 
